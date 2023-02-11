fc2e86799629ff6d87b15d3e260d016aea03f756
MaxSkippGriff <60075283+MaxSkippGriff@users.noreply.github.com>
2023-02-11 11:16:30 +0000

    Add css selector to grab daily pollution level

--- airquality/airquality/spiders/airpollution.py
+++ airquality/airquality/spiders/airpollution.py
@@ -4,7 +4,11 @@ import scrapy
 class AirpollutionSpider(scrapy.Spider):
     name = 'airpollution'
     allowed_domains = ['www.accuweather.com']
-    start_urls = ['https://www.accuweather.com/en/gb/birmingham/b5-5/air-quality-index/326966']
+    start_urls = ['https://www.accuweather.com/en/gb/kings-heath/b14-7/weather-forecast/321753']
 
     def parse(self, response):
-        pass
+        pollution_today = response.css('div.aq-number ::text').get().strip()
+
+        yield {
+            "pollution_today": pollution_today
+        }
