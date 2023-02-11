d8075bd3259d0b53b1df21fc5773166d1ead0175
MaxSkippGriff <60075283+MaxSkippGriff@users.noreply.github.com>
2023-02-11 18:45:09 +0000

    Extract date, day, idx, classification, and description

--- airquality/airquality/spiders/airpollution.py
+++ airquality/airquality/spiders/airpollution.py
@@ -7,30 +7,28 @@ class AirpollutionSpider(scrapy.Spider):
     start_urls = ['https://www.accuweather.com/en/gb/kings-heath/b14-7/air-quality-index/321753']
 
     def parse(self, response):
-
-
         # Get days of week from today to three days into the future
         days = response.css('p.day-of-week ::text').getall()
         # Remove duplicate 'today' item from list
         days.remove(days[0])
         # Loop through remaining days
-        # days = response.css('div.air-quality-daily-list').getall()
-        # days = response.css('div.air-quality-content ::text').getall()
         rows = response.css('div.air-quality-daily-list')
+        # 1. Get pollution index number
+        pollution_index = rows.css('div.aq-number ::text').getall()
+        # 2. Get classifcation e.g. fair, good
+        pollution_string = rows.css('p.category-text ::text').getall()
+        # 3. Get description of quality
+        pollution_descrip = rows.css('p.statement ::text').getall()
+        # 4. Get date 
+        dates = rows.css('p.date ::text').getall()
+        
+        i=0
         for day in days: 
-            # 1. Get pollution index number
-            pollution_index = rows.css('div.aq-number ::text').get().strip()
-            # 2. Get classifcation e.g. fair, good
-            pollution_string = rows.css('p.category-text ::text').get()
-            # 3. Get description of quality
-            pollution_descrip = rows.css('p.statement ::text').get().strip()
-            # 4. Get date 
-            date = rows.css('p.date ::text').get().strip()
-
-
             yield {
-                "Date": date,
-                "Pollution index": pollution_index,
-                "Classification": pollution_string,
-                "Description": pollution_descrip
+                "Date": dates[i],
+                "Day": day.strip(),
+                "Pollution index": pollution_index[i].strip(),
+                "Classification": pollution_string[i].strip(),
+                "Description": pollution_descrip[i].strip()
             }
+            i+=1
