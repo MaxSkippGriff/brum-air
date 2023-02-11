1e2e017c1a6e1f0356f7830f98f95282c1b28717
MaxSkippGriff <60075283+MaxSkippGriff@users.noreply.github.com>
2023-02-11 14:35:23 +0000

    Removing loop

--- airquality/airquality/spiders/airpollution.py
+++ airquality/airquality/spiders/airpollution.py
@@ -14,23 +14,23 @@ class AirpollutionSpider(scrapy.Spider):
         # Remove duplicate 'today' item from list
         # days.remove(days[0])
         # Loop through remaining days
-        #days = response.css('div.air-quality-daily-list').getall()
+        # days = response.css('div.air-quality-daily-list').getall()
         # days = response.css('div.air-quality-content ::text').getall()
 
-        for day in response.css('div.air-quality-daily-list'):
-            # 1. Get pollution index number
-            pollution_index = day.css('div.aq-number ::text').get().strip()
-            # 2. Get classifcation e.g. fair, good
-            pollution_string = day.css('p.category-text ::text').get()
-            # 3. Get description of quality
-            pollution_descrip = day.css('p.statement ::text').get().strip()
-            # 4. Get date 
-            date = day.css('p.date ::text').get().strip()
+        day = response.css('div.air-quality-daily-list')
+        # 1. Get pollution index number
+        pollution_index = day.css('div.aq-number ::text').get().strip()
+        # 2. Get classifcation e.g. fair, good
+        pollution_string = day.css('p.category-text ::text').get()
+        # 3. Get description of quality
+        pollution_descrip = day.css('p.statement ::text').get().strip()
+        # 4. Get date 
+        date = day.css('p.date ::text').get().strip()
 
 
-            yield {
-                "Date": date,
-                "Pollution index": pollution_index,
-                "Classification": pollution_string,
-                "Description": pollution_descrip
-            }
+        yield {
+            "Date": date,
+            "Pollution index": pollution_index,
+            "Classification": pollution_string,
+            "Description": pollution_descrip
+        }
