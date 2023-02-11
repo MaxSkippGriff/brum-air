99f5a6ad69cc5a614f9d9d254616956ef9924406
MaxSkippGriff <60075283+MaxSkippGriff@users.noreply.github.com>
2023-02-11 11:28:08 +0000

    Scrape description of pollution

--- airquality/airquality/spiders/airpollution.py
+++ airquality/airquality/spiders/airpollution.py
@@ -7,8 +7,10 @@ class AirpollutionSpider(scrapy.Spider):
     start_urls = ['https://www.accuweather.com/en/gb/kings-heath/b14-7/weather-forecast/321753']
 
     def parse(self, response):
-        pollution_today = response.css('div.aq-number ::text').get().strip()
+        pollution_num = response.css('div.aq-number ::text').get().strip()
+        pollution_string = response.css('p.category-text ::text').get()
 
         yield {
-            "pollution_today": pollution_today
+            "Pollution index": pollution_num,
+            "Description": pollution_string
         }
